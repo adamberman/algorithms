dca8a4df6ce98f9b5d0e91ab18b4d9aa56f6c5a8
adamberman <adamberman13@gmail.com>
2015-02-02 13:33:43 -0800

    write equal halves of array algorithm

--- equal_halves_of_array.rb
+++ equal_halves_of_array.rb
@@ -1,2 +1,20 @@
 def equal_halves_of_array(array)
+	sums = []
+	up_to_sum = 0
+	array.each do |el|
+		sums << [up_to_sum]
+		up_to_sum += el
+	end
+	down_to_sum = 0
+	i = array.length - 1
+	while i >= 0 do
+		sums[i] << down_to_sum
+		down_to_sum += el
+		i -= 1
+	end
+	sums.each_with_index do |values, idx|
+		return idx if values[0] == values[1]
+	end
+
+	-1
 end
