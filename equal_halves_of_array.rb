f7f706fe37d24940f3ec4c4002cb573b48955c91
adamberman <adamberman13@gmail.com>
2015-02-02 16:00:00 -0800

    better formatting

--- equal_halves_of_array.rb
+++ equal_halves_of_array.rb
@@ -1,17 +1,21 @@
 def equal_halves_of_array(array)
 	sums = []
 	up_to_sum = 0
+
 	array.each do |el|
 		sums << [up_to_sum]
 		up_to_sum += el
 	end
+
 	down_to_sum = 0
 	i = array.length - 1
+
 	while i >= 0 do
 		sums[i] << down_to_sum
 		down_to_sum += array[i]
 		i -= 1
 	end
+	
 	sums.each_with_index do |values, idx|
 		return idx if values[0] == values[1]
 	end
